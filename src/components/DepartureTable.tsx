8408d0adb0966ee213495f41fb9132b8b08e451f
reinole <ole.christian.reinhartsen@trumf.no>
2024-04-09 15:08:04 +0200

    Converted to Table and added styling

--- src/components/DepartureTable.tsx
+++ src/components/DepartureTable.tsx
@@ -1,36 +1,44 @@
 import { Departure, Call } from '../utils/types'
 
 export const DepartureTable = ({ data }: Departure) => {
-    if (!data) {
-        return <div>No data</div>
-    }
-
     const calulatedTimeDifference = (expected: string, actual: string) => {
         const expectedTime = new Date(expected)
         const actualTime = new Date(actual)
 
         const difference = expectedTime.getTime() - actualTime.getTime()
 
-        return Math.round(difference / 1000 / 60)
+        return Math.round(difference / 1000 / 60) > 0 ? Math.round(difference / 1000 / 60) : null
     }
 
     return (
-        <div>
-            {data.stopPlace.estimatedCalls.map((call: Call, i: number) => {
-                const { publicCode, name } = call.serviceJourney.journeyPattern.line
-                const hours = new Date(call.aimedArrivalTime).getHours()
-                const minutes = new Date(call.aimedArrivalTime).getMinutes()
+        <table>
+            <thead>
+                <tr>
+                    <th>Linje</th>
+                    <th>Navn</th>
+                    <th>Ankommer</th>
+                    <th>Forsinket (m)</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data && data.stopPlace.estimatedCalls.map((call: Call, i: number) => {
+                    const { publicCode, name } = call.serviceJourney.journeyPattern.line
+                    const hours = new Date(call.expectedArrivalTime).getHours()
+                    const hoursTwoDigits = hours < 10 ? `0${hours}` : hours
+                    const minutes = new Date(call.expectedArrivalTime).getMinutes()
+                    const minutesTwoDigits = minutes < 10 ? `0${minutes}` : minutes
+                    const minutesLate = calulatedTimeDifference(call.expectedArrivalTime, call.aimedArrivalTime)
 
-                return (
-                    <div key={call.serviceJourney.journeyPattern.line.id + i} style={{ display: "flex", justifyContent: "space-between", width: "45rem" }}>
-                        <span>{publicCode}</span>
-                        <span>{name}</span>
-                        <span>{hours}:{minutes}</span>
-                        <span>Forsinket med:{calulatedTimeDifference(call.expectedArrivalTime, call.aimedArrivalTime)} minutter</span>
-                    </div>
-                )
-            })}
-        </div>
+                    return (
+                        <tr key={call.serviceJourney.journeyPattern.line.id + i}>
+                            <td>{publicCode}</td>
+                            <td>{name}</td>
+                            <td>{hoursTwoDigits}:{minutesTwoDigits}</td>
+                            <td>{minutesLate}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
     )
-
 }
